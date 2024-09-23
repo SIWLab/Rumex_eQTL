13fe08a726730b074fd103159d82c59da503da0a
yuan <viciayuan@hotmail.com>
2024-09-23 13:56:04 -0400

    update

--- tensorqtl/tensorqtl_sex.sh
+++ tensorqtl/tensorqtl_sex.sh
@@ -21,7 +21,7 @@ plink --vcf $VCF --double-id --allow-extra-chr  \
 bgzip normalized_counts_ln.bed && tabix -p bed normalized_counts_ln.bed.gz
 
 
-# run tensorqtl
+# run tensorqtl with sex as covaraite
 plink_prefix_path=/ohta2/meng.yuan/rumex/eqtl/plink/sex
 expression_bed=/ohta2/meng.yuan/rumex/eqtl/tensorqtl/normalized_counts_ln.bed.gz
 prefix=sex
@@ -43,14 +43,6 @@ python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix} \
  #  * computing permutations
  #    processing phenotype 14549/14549
 
-
-plink_prefix_path=/ohta2/meng.yuan/rumex/eqtl/plink/sex
-expression_bed=/ohta2/meng.yuan/rumex/eqtl/tensorqtl/normalized_counts_ln.bed.gz
-prefix=sex
-covariates_file=/ohta2/meng.yuan/rumex/eqtl/tensorqtl/covariate_sex.txt
-interactions_file=/ohta2/meng.yuan/rumex/eqtl/tensorqtl/interaction_sex.txt
-
-
 # cis-QTL mapping: summary statistics for all variant-phenotype pairs
 python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix} \
     --covariates ${covariates_file} \
@@ -63,28 +55,38 @@ python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix} \
   # * cis-window: ±20,000
   # * checking phenotypes: 14804/14804
   #   ** dropping 255 phenotypes without variants in cis-window
-  # * Computing associations
-  #   Mapping chromosome A1
-  #   processing phenotype 4953/14549    time elapsed: 0.68 min
-  #   * writing output
-  #   Mapping chromosome A2
-  #   processing phenotype 9561/14549    time elapsed: 1.13 min
-  #   * writing output
-  #   Mapping chromosome A3
-  #   processing phenotype 12236/14549    time elapsed: 1.40 min
-  #   * writing output
-  #   Mapping chromosome A4
-  #   processing phenotype 14549/14549
 
 
-# add interaction term
-python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix}_interaction \
+# run tensorqtl without sex as covaraite
+plink_prefix_path=/ohta2/meng.yuan/rumex/eqtl/plink/sex
+expression_bed=/ohta2/meng.yuan/rumex/eqtl/tensorqtl/normalized_counts_ln.bed.gz
+prefix=nosex
+covariates_file=/ohta2/meng.yuan/rumex/eqtl/tensorqtl/covariate_nosex.txt
+# cis-QTL mapping: permutations
+python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix} \
+    --covariates ${covariates_file} \
+    --mode cis --window 20000
+
+python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix} \
+    --covariates ${covariates_file} \
+    --mode cis_nominal --window 20000
+
+
+# run tensorqtl with GxSex interaction term
+# the covariate file should not include sex???
+plink_prefix_path=/ohta2/meng.yuan/rumex/eqtl/plink/sex
+expression_bed=/ohta2/meng.yuan/rumex/eqtl/tensorqtl/normalized_counts_ln.bed.gz
+prefix=nosex
+covariates_file=/ohta2/meng.yuan/rumex/eqtl/tensorqtl/covariate_nosex.txt
+interactions_file=/ohta2/meng.yuan/rumex/eqtl/tensorqtl/interaction_sex.txt
+
+python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix}_inter \
     --covariates ${covariates_file} \
     --interaction ${interactions_file} \
     --mode cis_nominal --window 20000
 
-
-python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix}_interaction \
+# keep only top association
+python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix}_inter \
     --covariates ${covariates_file} \
     --interaction ${interactions_file} \
     --best_only \
@@ -99,15 +101,4 @@ python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix}_interactio
   # * cis-window: ±20,000
   # * checking phenotypes: 14804/14804
   #   ** dropping 255 phenotypes without variants in cis-window
-  # * Computing associations
-  #   Mapping chromosome A1
-  #   processing phenotype 4953/14549    time elapsed: 3.04 min
-  #   * writing output
-  #   Mapping chromosome A2
-  #   processing phenotype 9561/14549    time elapsed: 5.22 min
-  #   * writing output
-  #   Mapping chromosome A3
-  #   processing phenotype 12236/14549    time elapsed: 6.95 min
-  #   * writing output
-  #   Mapping chromosome A4
-  #   processing phenotype 14549/14549
+

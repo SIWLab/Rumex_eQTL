354a5f44d2f4f8fce8a254a01c9cd1e461d241e7
yuan <viciayuan@hotmail.com>
2025-04-17 21:50:55 -0400

    redo MAFs in FL auto X and PAR

--- eqtl/MAF/pheno_permute_fl.R
+++ eqtl/MAF/pheno_permute_fl.R
@@ -1,7 +1,7 @@
 setwd("/ohta2/meng.yuan/rumex/eqtl/tensorqtl/FL")
 args = commandArgs(trailingOnly=TRUE)
 i <- as.numeric(args[1])
-cnt <- read.table("normalized_counts_fln_auto.bed", header = T, check.names = F)
+cnt <- read.table("normalized_counts_fln_X_PAR.bed", header = T, check.names = F)
 col_names <- colnames(cnt)
 
 set.seed(1 + i) 
@@ -10,4 +10,10 @@ colnames(cnt_shuffled) <- col_names
 colnames(cnt_shuffled)[1] <- "#chr"
 write.table(cnt_shuffled, paste0("normalized_counts_fln_auto_", i, ".bed"), sep = "\t", row.names = FALSE, quote = FALSE)
 
+
+# might be worth merging the FL VCFs
+# otherwise this:
+normalized_counts_fln_X <- normalized_counts_fln %>% filter(chrom == "X") #1894
+normalized_counts_fln_PAR <- normalized_counts_fln %>% filter(chrom == "PAR") #1102
+normalized_counts_fln_auto <- normalized_counts_fln %>% filter(chrom != "PAR" & chrom != "X") #14634
 
